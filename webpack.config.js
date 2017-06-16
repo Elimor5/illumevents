32c2297bf226e1cba285efa080fd5625614b6d23
Eli Mordechai <emor0219@gmail.com>
2017-06-15 20:22:44 -0400

    add JS files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/index.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["es2015","react"]
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+}
