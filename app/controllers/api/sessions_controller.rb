a7704320292c96951a560cf1c95f3c83920c0ded
elimor5 <emor0219@gmail.com>
2018-06-03 22:46:09 -0400

    updated dependencies

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Api::SessionsController < ApplicationController
   def create
-		@user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
-
+    
     if @user
 			login(@user)
 			render "api/users/show"
